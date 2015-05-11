21f7e25a45d66db632fe9971609c7f2a52e613f5
mespinozas <mario.espinoza@gmail.com>
2015-05-11 04:16:01 -0300

    Auto date en sale

--- models/sale.js
+++ models/sale.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 
 var saleSchema = mongoose.Schema({
-  date:  {type: String, required:true},
+  date:  {type: Date, default: Date.now},
   store: {type: String, required:true},
   status: {type: String, required:true},
   client: {type: String, required:true},

2a34bea3b10625fa754d671dddfe5c3477afdfb5
mespinozas <mario.espinoza@gmail.com>
2015-06-26 14:54:23 -0300

    Revert "multiprice"
    This reverts commit 2b00b3491487e1523e47e457a10b3e9c4febe178.

--- models/product.js
+++ models/product.js
@@ -4,12 +4,7 @@ var mongoose = require('mongoose');
 
 var productSchema = mongoose.Schema({
   _name:  {type: String, required:true},
-  _price: [{
-      _units: {type: Number, required:true},
-      _store: {type: String, required:true},
-      _discount:  {type: Number, required:true},
-      _unitPrice: {type: Number, required:true}
-  }],
+  _price: {type: Number, required:true},
   _code: {type: String, index: {unique: true, dropDups: true}},
   _tags: [{ type: String, index: true }]
 

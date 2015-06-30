58bdf69934518525f91dfaa84a10fb58c3abb112
mespinozas <mario.espinoza@gmail.com>
2015-06-30 12:21:43 -0300

    apisecured

--- controllers/api/clients/index.js
+++ controllers/api/clients/index.js
@@ -1,21 +1,3 @@
-'use strict';
-
-var clientsLib = require('../../../lib/clientsLib');
-var mysecret = require('../../../config/secret');
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-
-module.exports = function (router) {
-    router.get('/', function (req, res) {
-        clientsLib.getAll(function(error, results){
-            res.setHeader('Access-Control-Allow-Origin','*');
-            if (error){
-                return res.status(500).json(error).end();
-            }
-            res.status(200).json(results).end();
-        });
-    });
-
     // /*Version Hasheada*/
     // router.post('/', function(req, res, next) {
     //     var user = new clientsLib.ClientModel();
@@ -46,14 +28,61 @@ module.exports = function (router) {
     //     });
     // });
 
+/**********/
+'use strict';
+
+var clientsLib = require('../../../lib/clientsLib');
+var mysecret = require('../../../config/secret');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+module.exports = function (router) {
+    router.get('/', function (req, res) {
+	//If access token was not sent in the query string ej: api/login?who=google&access_token=wrwerwqer
+        //Then return a 401
+        if(!req.query || !req.query.thirdparty || !req.query.userid || !req.query.accesstoken){
+            return res.status(401).end();
+        }
+
+	//Validate the access token with the third party (ej: google)
+        clientsLib.validateThirdPartyAccessToken(req.query.who, req.query.accesstoken, function(error){
+            if (error){
+                return res.status(401).end();
+            }
+
+            //Get the JWToken
+            clientsLib.getToken(req.query.userid, function(error, token){
+                return res.status(200).json({'token': token}).end();
+            });
+        });
+
+        clientsLib.getAll(function(error, results){
+            res.setHeader('Access-Control-Allow-Origin','*');
+            if (error){
+                return res.status(500).json(error).end();
+            }
+            res.status(200).json(results).end();
+        });
+    });
+
     router.post('/', function (req, res) {
         var data=req.body;
-        var username=data._username;
-        var password=data._password; //hash md5, over SSL
+        var username=data.username;
+        var password=data.password; //hash md5, over SSL
 
-        //clientsLib.findOne( ){
+        clientsLib.findOne(data.username, data.password, function(error, client){
+            if (error && error.message === 'NOT_FOUND'){
+                return res.status(401).json({message: 'USER_OR_PASSWORD_NOT_FOUND'}).end();
+            }
+            if (error){
+                return res.status(500).json(error).end();
+            }
+            console.log(client);
+            clientsLib.getToken(client.id, function(error, token){
+                return res.status(201).json({'token': token}).end();
+            });
+        });
 
-        //});//
         var newClient = req.body;
 
         clientsLib.create(newClient, function(error){
@@ -63,7 +92,6 @@ module.exports = function (router) {
             res.status(201).end();
         });
     });
-
 
     router.get('/:_mail', function (req, res) {
         var mail = req.params._mail;
@@ -78,8 +106,8 @@ module.exports = function (router) {
         });
     });
 
-    router.put('/:_id', function (req, res) {
-        var id = req.params._id;
+    router.put('/:id', function (req, res) {
+        var id = req.params.id;
         var newData = req.body;
         clientsLib.update(id, newData, function(error, client){
             if (error){
@@ -118,5 +146,4 @@ module.exports = function (router) {
             res.status(200).json(client).end();
         });
     });
-
 };
